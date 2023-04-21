7a396441f79fcc8e3c777ed6e34f07296b2247b8
kphed <kpcrypto69@gmail.com>
2023-04-21 05:20:10 -0400

    Clean up and format

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Modified from: https://github.com/folia-app/eip-2929 (thank you Folia App team).
 ## Instructions
 
 1. Install deps `npm install` or `yarn`
-2. Set private key of the account that will send t he transaction (i.e. call `LlamaAuctionHouse.settle_current_and_create_new_auction`)
+2. Set rpc URL, and private key of the account that will send the transaction (i.e. call `LlamaAuctionHouse.settle_current_and_create_new_auction`)
 3. Run program with `node index.js`
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ethers } = require("ethers");
+const { ethers } = require('ethers');
 const llamaAuctionHouseAbi = require('./llamaAuctionHouse.json');
 
 // @NOTE: Replace the two placeholder values below
@@ -9,9 +9,12 @@ const llamaAuctionHouse = '0xc5e5ca79d59c25a5f41e2aea4251f1c48419c2ab';
 const llamaGnosisSafe = '0x73eb240a06f0e0747c698a219462059be6aaccc8';
 
 async function run() {
-    let wallet = new ethers.Wallet(walletPrivateKey, provider)
-
-    const llamaAuctionHouseContract = new ethers.Contract(llamaAuctionHouse, llamaAuctionHouseAbi, wallet)
+    const wallet = new ethers.Wallet(walletPrivateKey, provider);
+    const llamaAuctionHouseContract = new ethers.Contract(
+        llamaAuctionHouse,
+        llamaAuctionHouseAbi,
+        wallet
+    );
 
     overrides = {
         gasLimit: 500_000,
@@ -22,18 +25,21 @@ async function run() {
                 // Gnosis Safe (receives ETH via `send`)
                 address: llamaGnosisSafe,
                 storageKeys: [
-                    "0x0000000000000000000000000000000000000000000000000000000000000000",
+                    '0x0000000000000000000000000000000000000000000000000000000000000000',
                 ],
             },
             {
                 // Gnosis Safe singleton (retrieved from contract creation tx https://etherscan.io/tx/0x4a754ccfb1ec3de7a59710a2386245f9d4f6eca9cd1a55bc678a3cd771f67f75)
-                address: "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
+                address: '0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552',
                 storageKeys: [],
             },
         ],
-    }
+    };
 
-    const settlementTx = await llamaAuctionHouseContract.settle_current_and_create_new_auction(overrides);
+    const settlementTx =
+        await llamaAuctionHouseContract.settle_current_and_create_new_auction(
+            overrides
+        );
 
     // Log the tx, and the recipient after 1 confirmation
     console.log(settlementTx);
